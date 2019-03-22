08577072c30da4a82ee42e67fa50bebb98eb6b5c
Ryan Kes <alrayyes@gmail.com>
2019-03-22 11:04:27 +0100

    remove dmenu font data and call dmenu-frequency instead of dmenu_run

--- config.h
+++ config.h
@@ -10,7 +10,6 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "SauceCodePro Nerd Font:style=Regular:pixelsize=18:antialias=true:autohint=true" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:style=Regular:pixelsize=18:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -146,7 +145,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "/home/alrayyes/.scripts/dmenu-frequency" };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
